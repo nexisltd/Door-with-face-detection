bcfd8cd4ae071fa50857743c45a04d852bbe5ff4
Nahidujjaman Hridoy <75487507+nhridoy@users.noreply.github.com>
2022-11-20 14:10:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def capture(read_frame_list, Global, worker_num):
     video_capture.release()
 
 def Door():
-    sleep_time=random.uniform(0.1,0.5)
+    sleep_time=random.uniform(0.1,1.5)
     time.sleep(sleep_time)
     conn = None
     zk = ZK(f'{os.getenv("ZK_IP")}', port=4370, timeout=5, password=f'{os.getenv("ZK_PASSWORD")}', force_udp=False,
